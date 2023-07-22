afba49dfb669f5ba5c66fc2e7a71161d4b768576
Hoperaisers <abdallahsafari@gmail.com>
2023-07-22 07:13:36 +0300

    corrects useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
 import "./App.css";
 function App() {
-  const [count, setCount] = useState("0");
+  const [count, setCount] = useState(0);
   function decreementCount() {
-    setCount(count - 1);
+    setCount((prevcount) => prevcount - 1);
   }
   function increementCount() {
-    setCount(count + 1);
+    setCount((prevcount) => prevcount + 1);
   }
   return (
     <>
